f32ad41670ede31277a0f2dc5c5d7faf66382345
Omkar Khamkar <omkar.khamkar18@gmail.com>
2026-02-24 23:49:13 +0530

    Add files via upload

---
+++ program.java
@@ -0,0 +1,16 @@
+class program{
+
+    public static void main(String [] args){
+       
+       int a = 2 , b = 5 , c = 10;
+        System.out.println("First number is :"+a);
+
+        System.out.println("Second number is :"+b);
+
+        System.out.println("Third number is :"+c);
+
+        System.out.println("Their addition is "+(a+b+c));
+
+        System.out.println("Their multipication is "+(a*b*c));
+    }
+}
