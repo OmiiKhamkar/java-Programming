715e5036d744ede1337b5049b236e2d6aa302160
Omkar Khamkar <omkar.khamkar18@gmail.com>
2026-02-24 23:49:34 +0530

    Add files via upload

---
+++ program2.java
@@ -0,0 +1,19 @@
+public class program2 {
+    public static void main(String[] args) {
+        int a = 25;
+        int square  = a*a;
+        int cube = a*a*a;
+
+        System.out.println("The given number is "+a);
+
+        System.out.println("The square is "+square);
+
+        System.out.println("The cube is "+cube);
+      
+
+
+    
+
+
+    }
+}
