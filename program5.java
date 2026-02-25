d547f9af6328ea90d01087797c8f01fc731f11fd
Omkar Khamkar <omkar.khamkar18@gmail.com>
2026-02-25 23:48:02 +0530

    Add files via upload

---
+++ program5.java
@@ -0,0 +1,15 @@
+
+class program5{
+    public static void main(String[] args) {
+        int tdays=800;
+        int y,days;
+        y=365;
+
+        System.out.println("Total days :"+(tdays));
+        System.out.println("Year :"+(tdays/y));
+        System.out.println("Remaining days :"+(tdays%y));
+
+
+    
+    }
+}
