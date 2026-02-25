cba63a5bf16cdccf326f26650eee1c2ba626d355
Omkar Khamkar <omkar.khamkar18@gmail.com>
2026-02-25 23:45:01 +0530

    Add files via upload

---
+++ program4.java
@@ -0,0 +1,19 @@
+public class program4 {
+    public static void main(String[] args) {
+        
+         
+        int x,y,z;
+
+        x=25;
+        y=18;
+        z=40;
+       
+        System.out.println("Quantity of Rs.100 :"+(x));
+        System.out.println("Quantity of Rs.50 :"+(y));
+        System.out.println("Quantity of Rs.10 :"+(z));
+
+        System.out.println("Total Amount :"+(x*100+y*50+z*10));
+
+    
+    }
+}
