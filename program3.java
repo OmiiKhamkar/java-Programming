6edee2dabdaf00f0b251e172a92e3da54c916719
Omkar Khamkar <omkar.khamkar18@gmail.com>
2026-02-25 23:44:32 +0530

    Add files via upload

---
+++ program3.java
@@ -0,0 +1,11 @@
+
+class program3 {
+
+    public static void main(String[] args) {
+        int km,m;
+        km=5;
+        System.out.println("Distance in kilometer :"+ km);
+        m=km*1000;
+        System.out.println("Distance in meters :"+(5*km));
+    }
+}
