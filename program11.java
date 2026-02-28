f70238f336a83b95ce8735deab462954194b7d90
Omkar Khamkar <omkar.khamkar18@gmail.com>
2026-02-28 12:20:08 +0530

    Add files via upload

---
+++ program11.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class program11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter co-ordinates of first point");
+        float x = sc.nextFloat();
+
+        System.out.println("Enter co-ordinates of first point");
+        float y = sc.nextFloat();
+
+        float lenth = Math.sqrt(((x*x-x)*(x*x-x))+((y*y-y)*(y*y-y)));
+
+
+    }
+}
