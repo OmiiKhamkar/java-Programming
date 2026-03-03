b4a1808a63a2244c142db6fc863e51a135aad0f9
Omkar Khamkar <omkar.khamkar18@gmail.com>
2026-03-03 20:32:11 +0530

    Add files via upload

---
+++ program20.java
@@ -0,0 +1,38 @@
+
+import java.util.Scanner;
+
+public class program20 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter number between 1 to 7:");
+        int num=sc.nextInt();
+        int day;
+
+        if(num>7 || num<1){
+            System.out.println("Wrong input");
+        }
+        else if(num==1){
+            System.out.println("Monday");
+        }
+         else if(num==2){
+            System.out.println("Tuesday");
+        }
+         else if(num==3){
+            System.out.println("Wensday");
+        }
+         else if(num==4){
+            System.out.println("Thusday");
+        }
+         else if(num==5){
+            System.out.println("Friday");
+        }
+         else if(num==6){
+            System.out.println("Saturday");
+        }
+         else {
+            System.out.println("Sunday");
+        }
+
+    }
+}
