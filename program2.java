40c274f8533f101910afd8268da075907970802d
Omkar Khamkar <omkar.khamkar18@gmail.com>
2026-03-04 23:33:07 +0530

    Exercise Question

--- program2.java
+++ program2.java
@@ -1,19 +1,18 @@
+
+import java.util.Scanner;
+
 public class program2 {
     public static void main(String[] args) {
-        int a = 25;
-        int square  = a*a;
-        int cube = a*a*a;
+     
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter the amount to be withdrawn");
+      int  amt=sc.nextInt();// 2890
 
-        System.out.println("The given number is "+a);
-
-        System.out.println("The square is "+square);
-
-        System.out.println("The cube is "+cube);
-      
-
-
-    
-
-
+       System.out.print("Rs. 100 notes => ");
+       int r100 = sc.nextInt();
+       System.out.print("Rs. 50 notes => ");
+       int r50 = sc.nextInt();
+       System.out.print("Rs. 10 notes => ");
+       int r10 = sc.nextInt();
     }
 }
