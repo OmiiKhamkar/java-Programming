77ec9e4c9c5ff2a7fab7753dd986b72bb446a316
Omkar Khamkar <omkar.khamkar18@gmail.com>
2026-03-11 22:34:56 +0530

    Add files via upload

---
+++ vowel.java
@@ -0,0 +1,26 @@
+package myfirstproject;
+
+import java.util.Scanner;
+
+public class vowel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner (System.in);
+		
+		Char = a,e,i,o,u;
+		System.out.println("Enter the character :");
+		
+		String ch = sc.next();
+		
+		if (ch==a||ch==e||ch==i||ch==o||ch==u)
+			System.out.println("This is vowel character");
+		else
+			System.out.println("This is not vowel character");
+		
+			
+
+	}
+
+}
