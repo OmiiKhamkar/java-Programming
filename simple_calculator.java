29bf382de88a260dad99211c2d912b88379a36a3
Omkar Khamkar <omkar.khamkar18@gmail.com>
2026-03-12 22:39:23 +0530

    Add files via upload

---
+++ simple_calculator.java
@@ -0,0 +1,24 @@
+package myfirstproject;
+
+import java.util.Scanner;
+
+public class simple_calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   
+		Scanner sc = new Scanner (System.in);
+		
+		System.out.println("Enter the first number :");
+		int a = sc.nextInt();
+		
+		System.out.println("Enter the first number :");
+		int b = sc.nextInt();
+		
+		System.out.println("Enter the operator number EX. +,-,*,/ :");
+		
+		if()
+			
+	}
+
+}
