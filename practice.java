5208c0ac9b651a15281330114c5a24443aabb905
Omkar Khamkar <omkar.khamkar18@gmail.com>
2026-03-14 23:07:15 +0530

    Add files via upload

--- practice.java
+++ practice.java
@@ -1,28 +1,33 @@
-package myfirstproject;
-
 import java.util.Scanner;
 
 public class practice {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter the number:");
+        int n = sc.nextInt();
+        int digit;
+        int even = 0,odd = 0;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner (System.in);
-		
-		System.out.println("Enter the number :");
-		int a = sc.nextInt();
-		
-		if(a%2==0&&a%3==0) {
-			
-			System.out.println("Even and divide by 3");
-			}
-		else if (a%2!=0&&a%5==0) {
-			System.out.println("Odd and divide by 5");
-		}
-		
-		else {
-			System.out.println("None");
-		}
-		
-	}
+         while (n>0) { 
+            digit = n%10;
+           // System.out.print(digit+"\n");
+       
+            n = n/10;
 
-}
+            if(digit%2==0){
+               even = digit+1;
+              // System.out.print(even+", ");
+            } // if
+            else{
+               odd = digit-1;
+              // System.out.print(odd+", ");
+            } // else
+                 System.out.print(even);
+          System.out.print(odd);
+               
+        } // whileloop
+         
+       
+    } //main
+} // class
