10723a7eccd6da9bfa42a482c2ec56a9fc9db301
Omkar Khamkar <omkar.khamkar18@gmail.com>
2026-03-18 22:47:58 +0530

    Add files via upload

---
+++ pattern16.java
@@ -0,0 +1,17 @@
+public class pattern1 {
+    public static void main(String[] args) {
+        int i = 1;
+        int j;
+        while (i<=6) { 
+
+            j = 1;
+            while (j<=10) { 
+                System.out.print(j+" ");
+                j++;
+            }
+
+            System.out.println();
+            i++;
+        }
+    }
+}
