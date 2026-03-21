fc9f86aabd084384ee60e451a667845bf0f088e7
Omkar Khamkar <omkar.khamkar18@gmail.com>
2026-03-21 22:17:42 +0530

    Add files via upload

---
+++ Array2.java
@@ -0,0 +1,19 @@
+public class Array2 {
+    public static void main(String[] args) {
+        int a[]= {11,22,33,44};
+        char b[]= {'H','e','l','l','o'};
+        String c[]={"Mahendra","Virat","Rohit","Hardik","Jasprit"};
+        System.out.print("Contnet of array a is ");
+        for (int e : a) {
+            System.out.print(e+" ");
+        }
+        System.out.print("\nContnet of array b is ");
+        for (char f : b) {
+            System.out.print(f+" ");
+        }
+        System.out.print("\nContnet of array c is ");
+        for (String g : c) {
+            System.out.print(g+" ");
+        }
+    }
+}
