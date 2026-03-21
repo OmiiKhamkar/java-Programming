7b0d3e98cbfadbc897003d81ea01fa5067fa23cb
Omkar Khamkar <omkar.khamkar18@gmail.com>
2026-03-21 22:17:29 +0530

    Add files via upload

---
+++ Array1.java
@@ -0,0 +1,19 @@
+public class Array1 {
+    public static void main(String[] args) {
+        int a[]= {10,20,30,40,50,60};
+        char b[]= {'I','N','D','I','A'};
+        String c[]={"Pune","Mumbai","Nashik","Nagpur","Nanded"};
+        System.out.print("Contnet of array a is ");
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i]+" ");
+        }
+        System.out.print("\nContnet of array b is ");
+        for (int i = 0; i < b.length; i++) {
+            System.out.print(b[i]+" ");
+        }
+        System.out.print("\nContnet of array c is ");
+        for (int i = 0; i < c.length; i++) {
+            System.out.print(c[i]+" ");
+        }
+    }
+}
