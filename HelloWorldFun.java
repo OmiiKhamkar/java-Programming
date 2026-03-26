f5fcbe77929fb02fe3e367c24dd9f68783cba718
Omkar Khamkar <omkar.khamkar18@gmail.com>
2026-03-26 23:40:18 +0530

    Started The Function

---
+++ HelloWorldFun.java
@@ -0,0 +1,11 @@
+public class HelloWorldFun {
+    public static void main(String[] args) {
+        
+        display();
+
+    }
+    static void display(){
+        System.out.println("Hello World !");
+    }
+}
+                                                                       
